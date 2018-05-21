3d90acd2020f941e439a6dcb258865acff442fa1
Mark Ryan <676615845@qq.com>
2018-05-21 20:14:23 +0800

    default

--- appm/cli.py
+++ appm/cli.py
@@ -16,7 +16,7 @@ def register(app):
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
         if os.system(
-                'pybabel init -i messages.pot -d app/translations -l ' + lang):
+                'pybabel init -i messages.pot -d appm/translations -l ' + lang):
             raise RuntimeError('init command failed')
         os.remove('messages.pot')
 
@@ -25,12 +25,12 @@ def register(app):
         """Update all languages."""
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
-        if os.system('pybabel update -i messages.pot -d app/translations'):
+        if os.system('pybabel update -i messages.pot -d appm/translations'):
             raise RuntimeError('update command failed')
         os.remove('messages.pot')
 
     @translate.command()
     def compile():
         """Compile all languages."""
-        if os.system('pybabel compile -d app/translations'):
+        if os.system('pybabel compile -d appm/translations'):
             raise RuntimeError('compile command failed')
